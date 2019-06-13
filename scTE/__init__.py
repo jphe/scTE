52a299c80e9df9851bd5b0a73beadefe0dda1f7b
jphe <34993687+jphe@users.noreply.github.com>
2019-06-13 16:42:25 +0800

    Update __init__.py

--- scTE/__init__.py
+++ scTE/__init__.py
@@ -1,7 +1,7 @@
 
 __version__ = "1.0"
 
-from .miniglbase import genelist, location, glload
-#import miniglbase
+# from .miniglbase import genelist, location, glload
+# import miniglbase
 
-__all__ = ["genelist", "location", "glload"]
+# __all__ = ["genelist", "location", "glload"]
