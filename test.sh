8b77499ef823292e3c58906711962f39400c763d
jphe <260865361@qq.com>
2019-06-21 20:34:18 +0800

    modified test data

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 
 scTE_build -g mm10 -te Data/TE.bed -gene Data/Gene.gtf -o Data/test
 
-scTE -i Data/test.bam -p 12 --min_genes 1 -o out --genome mm10 -m exclusive -x  Data/test.exclusive.glb
+scTE -i Data/test.bam -p 12 --min_genes 1 -o out --genome mm10 -m exclusive -x  Data/test.exclusive.idx
 
