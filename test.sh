560fa49ca5d07141f9574f62570ff9616c45048e
jphe <34993687+jphe@users.noreply.github.com>
2019-06-24 14:31:11 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 
-scTE_build -g mm10 -te Data/TE.bed -gene Data/Gene.gtf -o Data/test
+scTE_build -g mm10 -te Data/TE.bed -gene Data/Gene.gtf -o Data/test -m exclusive
 
-scTE -i Data/test.bam -p 12 --min_genes 1 -o out --genome mm10 -m exclusive -x  Data/test.exclusive.idx
+scTE -i Data/test.bam -p 12 --min_genes 1 -o out --genome mm10 -x  Data/test.exclusive.idx
 
