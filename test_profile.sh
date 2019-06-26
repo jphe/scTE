797772260f85aebb9f8569bcd64cc4ad9183ac22
oaxiom <oaxiom@gmail.com>
2019-06-27 07:48:09 +0800

    added profiling code, but test is broken as test.bam is corrupt

---
+++ test_profile.sh
@@ -0,0 +1,5 @@
+
+python3 -m cProfile -s cumtime bin/scTE_build -g mm10 -te Data/TE.bed -gene Data/Gene.gtf -o Data/test >profile_scTE_build.txt
+
+python3 -m cProfile -s cumtime bin/scTE -i Data/test.bam -p 2 --min_genes 1 -o out --genome mm10 -x Data/test.exclusive.idx >profile_scTE.txt
+
