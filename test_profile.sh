dbdbc09999c105c777f7c08af52be30fe197a400
oaxiom <oaxiom@gmail.com>
2019-07-01 13:57:04 +0800

    removed the profile script from here

--- test_profile.sh
+++
@@ -1,5 +0,0 @@
-
-python3 -m cProfile -s cumtime bin/scTE_build -g mm10 -te Data/TE.bed -gene Data/Gene.gtf -o Data/test >profile_scTE_build.txt
-
-python3 -m cProfile -s cumtime bin/scTE -i Data/test.bam -p 1 --min_genes 1 -o out --genome mm10 -x Data/test.exclusive.idx >profile_scTE.txt
-
