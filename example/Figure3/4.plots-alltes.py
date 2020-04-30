e82b8a8413db9583ad5beb5d83f6cc6649a872f3
oaxiom <oaxiom@gmail.com>
2020-04-30 09:24:06 +0800

    TE plotting

--- example/Figure3/4.plots-alltes.py
+++ example/Figure3/4.plots-alltes.py
@@ -18,7 +18,7 @@ adata = sc.read('./learned.h5ad')
 print(adata)
 all_genes = adata.var['n_cells'].index # gene names are stored in the index
 
-TEs = genelist(filename='../../TE_genes_id.mm10.txt.gz', format={'name': 0, 'force_tsv': True}, gzip=True)
+TEs = genelist(filename='TE_genes_id.mm10.txt.gz', format={'name': 0, 'force_tsv': True}, gzip=True)
 
 #merker_tes = ['ID2', 'MER5C1', 'MER34B-int', 'MER63D', 'MT2A']
 #sc.pl.stacked_violin(adata, var_names=merker_tes, groupby='leiden_r0.2', rotation=90, show=False, save='tes.pdf')
